399d4e5eadf389fffa7a60a16436af0ff3c0e971
Manvi <manvib7423@gmail.com>
2023-08-14 14:27:32 -0400

    Initial Commit

---
+++ practiceProject/practiceProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceProjectApp.swift
+//  practiceProject
+//
+//  Created by Manvi Boppana on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct practiceProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
